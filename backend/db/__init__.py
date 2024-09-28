eedd1e0511c094aaa0a842266bff897c08240e28
aiker <test@example.com>
2024-09-28 12:58:38 +0300

    init: db

---
+++ backend/db/__init__.py
@@ -0,0 +1,2 @@
+from .create_db import create_tables
+from .models import * # импорт моделей
---
+++ backend/db/create_db.py
@@ -0,0 +1,30 @@
+import os
+import psycopg2
+
+
+def create_tables():
+    commands = (
+        #sql запрос на создание таблиц бд
+    )
+
+    try:
+        with psycopg2.connect(
+            database="postgres", user="postgres", host="127.0.0.1", password="postgres"
+        ) as conn:
+            with conn.cursor() as cur:
+                for command in commands:
+                    try:
+                        cur.execute("BEGIN;")
+                        cur.execute(command)
+                        cur.execute("COMMIT;")
+                    except (psycopg2.DatabaseError, Exception) as error:
+                        print(f"Ошибка при выполнении команды: {command}")
+                        print(error)
+                        cur.execute("ROLLBACK;")
+    except (psycopg2.DatabaseError, Exception) as conn_error:
+        print("Ошибка подключения к базе данных")
+        print(conn_error)
+    
+
+
+
