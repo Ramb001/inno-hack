9abbbfe26adebd111d6d110fa084765f78821b21
Dania <danila.lusuy@gmail.com>
2024-09-28 21:58:36 +0300

    feat: add

--- frontend/src/app/providers/app-router.tsx
+++ frontend/src/app/providers/app-router.tsx
@@ -2,6 +2,7 @@ import { MainPage } from "@/pages/main";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Layout } from "../layout/layout";
 import { LoginPage } from "@/pages/login";
+import { BoardPage } from "@/pages/board";
 
 export const createRouter = () =>
   createBrowserRouter([
@@ -13,12 +14,17 @@ export const createRouter = () =>
       path: "/login",
       element: <LoginPage />,
     },
+
     {
       element: <Layout />,
       children: [
         {
           path: "/main",
           element: <MainPage />,
+        },
+        {
+          path: "/board/:boardId",
+          element: <BoardPage />,
         },
       ],
     },
--- frontend/src/components/organization/organization-item/ui/organization-item.tsx
+++ frontend/src/components/organization/organization-item/ui/organization-item.tsx
@@ -1,12 +1,23 @@
+import { Link, useNavigate } from "react-router-dom";
+import { OrganizationDelete } from "../../organization-delete";
+
 type OrganizationItemProps = {
   name: string;
+  organizationId: number;
 };
 export const OrganizationItem = (props: OrganizationItemProps) => {
-  const { name } = props;
+  const { name, organizationId } = props;
+
   return (
-    <div className="hover:bg-gray-100 p-3 hover:cursor-pointer flex items-center hover:font-medium hover:rounded-xl gap-4">
-      <span className="w-2 h-2 rounded-full bg-green-500" />
-      <p>{name}</p>
+    <div className="hover:bg-gray-100 p-3 hover:cursor-pointer flex items-center justify-between hover:font-medium hover:rounded-xl gap-4">
+      <Link
+        className="flex items-center gap-4"
+        to={`/board/:${organizationId}`}
+      >
+        <span className="w-2 h-2 rounded-full bg-green-500" />
+        <p>{name}</p>
+      </Link>
+      <OrganizationDelete id={organizationId} />
     </div>
   );
 };
