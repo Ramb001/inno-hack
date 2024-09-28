1e63ee1e62c97c25a6e7ef56c84600122a08aa82
moto_moto <ustinovilia34@gmail.com>
2024-09-28 18:15:00 +0300

    feat: Adding login page

--- frontend/src/app/providers/app-router.tsx
+++ frontend/src/app/providers/app-router.tsx
@@ -1,14 +1,23 @@
 import { MainPage } from "@/pages/main";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Layout } from "../layout/layout";
+import { LoginPage } from "@/pages/login";
 
 export const createRouter = () =>
   createBrowserRouter([
     {
+      path: "/",
+      element: <Navigate to="/login" />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
       element: <Layout />,
       children: [
         {
-          path: "/",
+          path: "/main",
           element: <MainPage />,
         },
       ],
