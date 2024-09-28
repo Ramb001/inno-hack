b393c4ba406b952273ef7151af1916f3e4ffe988
Ramb001 <artem.rmb@bk.ru>
2024-09-28 14:14:14 +0300

    fix: fix

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,22 @@
 import logging
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from db import create_tables
-from routers import users, organizations 
+from routers import users, organizations
 
+# Set up logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-
 app = FastAPI()
+
+# Include routers
 app.include_router(users.router)
 app.include_router(organizations.router)
 
+# CORS configuration
 origins = ["*"]
 
 app.add_middleware(
@@ -28,9 +30,14 @@ app.add_middleware(
 
 @app.on_event("startup")
 def startup_event():
-    logging.info("Creating tables...")
-    create_tables_result = create_tables()
-    if create_tables_result:
-        logging.info("Tables created")
-    else:
-        logging.error("Failed to create tables")
+    logging.info("Starting application...")
+
+    try:
+        logging.info("Creating tables...")
+        create_tables_result = create_tables()
+        if create_tables_result:
+            logging.info("Tables created successfully")
+        else:
+            logging.error("Failed to create tables")
+    except Exception as e:
+        logging.error(f"An error occurred during startup: {e}")
