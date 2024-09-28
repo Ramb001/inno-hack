8f637462d2ddabd89f305f00a3d783f775aa519c
Ramb001 <artem.rmb@bk.ru>
2024-09-28 19:27:15 +0300

    fix: fix email

--- backend/utils/notificatior.py
+++ backend/utils/notificatior.py
@@ -9,40 +9,21 @@ import smtplib
 
 class EmailNotificatior:
     def __init__(self, message: str, receivers: list[str]):
-        self.smtp = smtplib.SMTP_SSL("smtp.yandex.ru", 465, timeout=10)
         self.message = message
         self.email = os.getenv("EMAIL")
         self.password = os.getenv("PASSWORD")
         self.receivers = receivers
 
     def send_email(self):
-        self.message["Subject"] = Header("Важное!!!", "utf-8")
-        self.message["From"] = self.email
-        self.message["To"] = ", ".join(self.receivers)
-
-        try:
-            with self.smtp as server:
-                server.login(self.email, self.password)
-                server.sendmail(self.email, self.receivers, self.message)
-                logging.info(f"Письмо успешно отправлено {self.receivers}")
-                server.quit()
-        except Exception as ex:
-            logging.error(f"Ошибка при отправке письма: {ex}")
-
-    def send_ya_mail(self):
-        login = "kotov1548@yandex.ru"
-        password = "zntvowvgustfrcgh"  # Берем пароль из переменной окружения
-
-        # Создание MIME-сообщения
         msg = MIMEText(self.message, "plain", "utf-8")
         msg["Subject"] = Header("Важное!!!", "utf-8")
-        msg["From"] = login
+        msg["From"] = self.email
         msg["To"] = ", ".join(self.receivers)
 
         try:
             with smtplib.SMTP_SSL("smtp.yandex.ru", 465, timeout=10) as server:
-                server.login(login, password)
-                server.sendmail(login, self.receivers, msg.as_string())
+                server.login(self.email, self.password)
+                server.sendmail(self.email, self.receivers, msg.as_string())
                 print("Письмо успешно отправлено!")
         except Exception as ex:
             print(f"Ошибка при отправке письма: {ex}")
