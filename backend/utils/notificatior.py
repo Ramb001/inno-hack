abc0394f62c421833b9859c64ebec231d3ee6ae4
Ramb001 <artem.rmb@bk.ru>
2024-09-28 18:59:26 +0300

    fix: fix email

--- backend/utils/notificatior.py
+++ backend/utils/notificatior.py
@@ -1,6 +1,6 @@
 import os
 import datetime
-
+import logging
 from smtplib import SMTP
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -19,5 +19,8 @@ class EmailNotificatior:
         self.smtp.login(self.email, self.password)
         date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
         msg = f"From: {self.email}\nTo: {self.receiver}\nSubject: New task was added\nDate: {date}\n\n{self.message}"
-        self.smtp.sendmail(self.email, self.receiver, msg)
+        try:
+            self.smtp.sendmail(self.email, self.receiver, msg)
+        except Exception as e:
+            logging.error(e)
         self.smtp.quit()
