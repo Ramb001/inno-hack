b6796caeddcb16b3602aaab6f87ae552c0330892
Ramb001 <artem.rmb@bk.ru>
2024-09-28 14:16:46 +0300

    fix: fix

--- backend/db/create_db.py
+++ backend/db/create_db.py
@@ -37,7 +37,7 @@ def create_tables():
         with psycopg2.connect(
             database="postgres",
             user="postgres",
-            host="postgres",
+            host="0.0.0.0",
             port="5432",
             password="postgres",
         ) as conn:
